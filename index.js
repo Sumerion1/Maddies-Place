02c7c8b44f51ef7e38e4d68d3e679f5871584922
Sumerion <pastorskid8@gmail.com>
2019-09-23 20:00:38 -0700

    initiate commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//Express +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const express = require('express');
+const helmet = require('helmet');
+const path = require('path') ;
+const PORT = process.env.PORT || 3000;
+const app = express(); 
+app.use(helmet())
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json()); 
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'blank'}));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, '../views'));
+app.use(express.static('static'));
+let server = app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+app.server = server;
+
+
+app.get('/',function(req,res){
+    res.render('home');
+})
